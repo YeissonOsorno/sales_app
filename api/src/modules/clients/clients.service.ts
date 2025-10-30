2a4b694773273b587932e4e58268c0fd65fbb5cd
Yeisson Osorno <yeisson403@gmail.com>
2025-10-30 12:34:19 -0500

    update structure

--- api/src/modules/clients/clients.service.ts
+++ api/src/modules/clients/clients.service.ts
@@ -1,26 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable , Inject} from '@nestjs/common';
 import { CreateClientDto } from './dto/create-client.dto';
 import { UpdateClientDto } from './dto/update-client.dto';
-
+import { DataSource, Repository } from 'typeorm';
+import { Client } from 'src/common/models/client.entity';
 @Injectable()
 export class ClientsService {
-  create(createClientDto: CreateClientDto) {
-    return 'This action adds a new client';
+  private clientRepo: Repository<Client>;
+  constructor(
+    @Inject("DATA_SOURCE") private dataSource: DataSource
+  ) {
+    this.clientRepo = this.dataSource.getRepository(Client);
+  }
+
+  async create(createClientDto: CreateClientDto) {
+    const client = this.clientRepo.create(createClientDto);
+    return this.clientRepo.save(client);    
   }
 
   findAll() {
-    return `This action returns all clients`;
+    return this.clientRepo.find();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} client`;
+    return this.clientRepo.findOneBy({id});
   }
 
-  update(id: number, updateClientDto: UpdateClientDto) {
-    return `This action updates a #${id} client`;
+  async update(id: number, updateClientDto: UpdateClientDto) {
+     await this.clientRepo.update(id, updateClientDto);
+    return this.findOne(id);
   }
 
   remove(id: number) {
-    return `This action removes a #${id} client`;
+    return this.clientRepo.delete(id);
   }
 }
--- api/src/modules/clients/dto/create-client.dto.ts
+++ api/src/modules/clients/dto/create-client.dto.ts
@@ -1,1 +1,15 @@
-export class CreateClientDto {}
+
+import { IsString, IsOptional } from 'class-validator';
+
+export class CreateClientDto {
+  @IsString()
+  rut: string;
+  
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @IsString()
+  address?: string;
+
+}
